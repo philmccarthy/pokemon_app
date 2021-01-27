cbbee5261852e166147dcb2ca2a8dc88ad67a96a
Phil McCarthy <philmcc2631@gmail.com>
2021-01-27 10:30:55 -0700

    Add initial working API call for pokemon search

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,5 +1,9 @@
 class PokemonController < ApplicationController
   def index
     pokemon = params[:pokemon]
+    conn = Faraday.new('https://pokeapi.co')
+    require 'pry'; binding.pry
+    response = conn.get("/api/v2/pokemon-form/#{pokemon}/")
+    @pokemon = JSON.parse(response.body, symbolize_names: true)
   end
 end
